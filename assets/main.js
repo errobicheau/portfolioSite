10e6b3b09a15bf3b18a07fc1fce05e963532da8c
Eric Robicheau <118728856+errobicheau@users.noreply.github.com>
2023-04-26 16:19:21 -0400

    Update skills sections of portfolio.html, addition of twitter icon contact section of  portfolio.html

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+function hover(element) {
+    element.setAttribute('src','assets/images/websiteTest.gif')
+}
+
+function unhover(element) {
+    element.setAttribute('src','assets/images/BOMDemo.jpg')
+    
+}
